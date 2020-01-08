2e823d1fb775f16ae220bc91c1833c3a5c991e07
Mostafa Hedawa <Mostafa.Hedawa@orascom.com>
2020-01-08 02:48:58 +0200

    adding api

---
+++ TaskBE/ProductAPI/ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Product.Domain.Entities;
+using Product.Services.IServices;
+
+namespace Product.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        #region Properties
+        private readonly IProductService _productService;
+        #endregion
+
+        #region Constructor
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        #endregion
+
+        #region Methods
+        // GET: api/Product
+        [HttpGet]
+        public  Task<IEnumerable<Products>> GetAsync()
+        {
+            return  _productService.ListAsync();
+        }
+
+        // GET: api/Product/5
+        [HttpGet("{id}", Name = "Get")]
+        public Task<Products> Get(int id)
+        {
+            return _productService.GetByIdAsync(id);
+        }
+
+        // POST: api/Product
+        [HttpPost]
+        public Task<Products> Post([FromBody] Products entity)
+        {
+            return _productService.CreateAsync(entity);
+        }
+
+        // PUT: api/Product/5
+        [HttpPut("{id}")]
+        public Task<Products> Put(int id, [FromBody] Products entity)
+        {
+            return _productService.UpdateAsync(id,entity);
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public Task<Products> Delete(int id)
+        {
+            return _productService.Delete(id);
+        }
+        #endregion
+    }
+}
---
+++ TaskBE/ProductAPI/ProductAPI/Startup.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Product.Infrastructure.Context;
+using Product.Infrastructure.Repository;
+using Product.Services.IRepositoriey;
+using Product.Services.IServices;
+using Product.Services.Services;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace ProductAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                 .AddJsonOptions( options => options.SerializerSettings.ReferenceLoopHandling =        Newtonsoft.Json.ReferenceLoopHandling.Ignore
+    );
+
+            services.AddDbContext<ProductDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IProductRepository, ProductRepositroy>();
+            services.AddScoped<IProductService, ProductService>();
+
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new Info { Title = "Product API", Version = "V1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "post API V1");
+            });
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
